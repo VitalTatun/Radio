420342b2e96dc9139ba7e451285a265ecdc53d29
VitalTatun <91216876+VitalTatun@users.noreply.github.com>
2026-03-03 13:21:41 +0300

    Initial Commit

---
+++ Radio/RadioApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadioApp.swift
+//  Radio
+//
+//  Created by Vital Tatun on 3.03.26.
+//
+
+import SwiftUI
+
+@main
+struct RadioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
